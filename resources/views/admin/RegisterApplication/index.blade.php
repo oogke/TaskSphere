4c5c21da3c670b47889bfb3b989da4e579e0f7fa
oogke <actlikeanut@gmail.com>
2025-01-30 09:56:08 +0545

    jan 30

--- resources/views/admin/RegisterApplication/index.blade.php
+++ resources/views/admin/RegisterApplication/index.blade.php
@@ -187,9 +187,10 @@
 </head>
 
 <body>
-<div id="toast-container" aria-live="polite" aria-atomic="true" class="position-fixed top-0 end-0 p-3" style="z-index: 11">
-  
-</div>
+    <div id="toast-container" aria-live="polite" aria-atomic="true" class="position-fixed top-0 end-0 p-3"
+        style="z-index: 11">
+
+    </div>
     <section class="adapplication">
         <h1 id="adapplication-head">Employee Register Application</h1>
         <div id="adapplication-table">
@@ -215,89 +216,94 @@
                             <td>{{ $application['email'] }}</td>
                             <td>{{ $application['created_at'] }}</td>
                             <td>
-                            <a href="" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#acceptModal" data-application-id="{{ $application['id'] }}">Accept</a>
-                            <a href="" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#rejectModal" data-application-id="{{ $application['id'] }}">Reject</a>
+                                <a href="" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#acceptModal"
+                                    data-application-id="{{ $application['id'] }}">Accept</a>
+                                <a href="" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#rejectModal"
+                                    data-application-id="{{ $application['id'] }}">Reject</a>
 
                             </td>
-    </tr>
-                    
-                    
+                        </tr>
+
+
                     @endforeach
 
-        <!-- secret code Modal -->
-        <div class="modal fade" id="scodeModal" data-bs-backdrop="static" data-bs-keyboard="false"
-                                tabindex="-1" aria-labelledby="singlePostLabel" aria-hidden="true">
-                                <div class="modal-dialog">
-                                    <div class="modal-content">
-                                        <div class="modal-header">
-                                            <h5 class="modal-title" id="singlePostLabel">Enter the secret code</h5>
-                                            <button type="button" class="btn-close" data-bs-dismiss="modal"
-                                                aria-label="Close"></button>
-                                        </div>
-                                        <div class="modal-body">
-                                            <input type="Password" id="scode" placeholder="secret code">
-                                            <input type="hidden" name="applicationId"  id="applicationIdInput">
-                                        </div>
-                                        <div class="modal-footer">
-                                            <button type="button" class="btn btn-success scodeEnter">okay</button>
-                                            <button type="button" class="btn btn-secondary"
-                                                data-bs-dismiss="modal" id="dismissModal">Close</button>
-                                        </div>
+                    <!-- secret code Modal -->
+                    <div class="modal fade" id="scodeModal" data-bs-backdrop="static" data-bs-keyboard="false"
+                        tabindex="-1" aria-labelledby="singlePostLabel" aria-hidden="true">
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title" id="singlePostLabel">Enter the secret code</h5>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                        aria-label="Close"></button>
+                                </div>
+                                <div class="modal-body">
+                                    <input type="Password" id="scode" placeholder="secret code">
+                                    <input type="hidden" name="applicationId" id="applicationIdInput">
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-success scodeEnter">okay</button>
+                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal"
+                                        id="dismissModal">Close</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- secret code Modal -->
+
+
+                    <!-- confirm reject -->
+                    <div class="modal fade" id="rejectModal" data-bs-backdrop="static" data-bs-keyboard="false"
+                        tabindex="-1" aria-labelledby="deleteLabel" aria-hidden="true">
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+                                <div class="modal-header">
+
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                        aria-label="Close"></button>
+                                </div>
+                                <div class="modal-body">
+                                    <div class="single-data" width="100%" height="100%">
+                                        Are you sure you want to reject?
                                     </div>
                                 </div>
-                            </div>
-                            <!-- secret code Modal -->
-
-
-                <!-- confirm reject -->
-                <div class="modal fade" id="rejectModal" data-bs-backdrop="static" data-bs-keyboard="false"
-                    tabindex="-1" aria-labelledby="deleteLabel" aria-hidden="true">
-                    <div class="modal-dialog">
-                        <div class="modal-content">
-                            <div class="modal-header">
-
-                                <button type="button" class="btn-close" data-bs-dismiss="modal"
-                                    aria-label="Close"></button>
-                            </div>
-                            <div class="modal-body">
-                                <div class="single-data" width="100%" height="100%">
-                                    Are you sure you want to reject?
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary"
+                                        data-bs-dismiss="modal">close</button>
+                                    <button type="button" class="btn btn-primary rejectConfirmBtn">Reject</button>
                                 </div>
-                            </div>
-                            <div class="modal-footer">
-                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">close</button>
-                                <button type="button" class="btn btn-primary rejectConfirmBtn" >Reject</button>
                             </div>
                         </div>
                     </div>
-                </div>
 
-                <!-- confirm reject -->
+                    <!-- confirm reject -->
 
-                <!--confirm accept -->
-                <div class="modal fade" id="acceptModal" data-bs-backdrop="static" data-bs-keyboard="false"
-                    tabindex="-1" aria-labelledby="deleteLabel" aria-hidden="true">
-                    <div class="modal-dialog">
-                        <div class="modal-content">
-                            <div class="modal-header">
+                    <!--confirm accept -->
+                    <div class="modal fade" id="acceptModal" data-bs-backdrop="static" data-bs-keyboard="false"
+                        tabindex="-1" aria-labelledby="deleteLabel" aria-hidden="true">
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+                                <div class="modal-header">
 
-                                <button type="button" class="btn-close" data-bs-dismiss="modal"
-                                    aria-label="Close"></button>
-                            </div>
-                            <div class="modal-body">
-                                <div class="single-data" width="100%" height="100%">
-                                    Are you sure you want to Accept?
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                        aria-label="Close"></button>
                                 </div>
-                            </div>
-                            <div class="modal-footer">
-                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">close</button>
-                                <button type="button" class="btn btn-primary acceptConfirmBtn"  data-bs-toggle="modal"
-                                    data-bs-target="#scodeModal" data-bs-dismiss="modal">Accept</button>
+                                <div class="modal-body">
+                                    <div class="single-data" width="100%" height="100%">
+                                        Are you sure you want to Accept?
+                                    </div>
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary"
+                                        data-bs-dismiss="modal">close</button>
+                                    <button type="button" class="btn btn-primary acceptConfirmBtn"
+                                        data-bs-toggle="modal" data-bs-target="#scodeModal"
+                                        data-bs-dismiss="modal">Accept</button>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-                <!--confirm accept -->
+                    <!--confirm accept -->
                 </tbody>
 
 
@@ -313,102 +319,90 @@
 
     <script>
 
-const acceptModal = document.getElementById('acceptModal');
-acceptModal.addEventListener('show.bs.modal', (event) => {
-  const button = event.relatedTarget; 
-  const applicationId = button.dataset.applicationId; 
-  const acceptBtn = document.querySelector(".acceptConfirmBtn");
-  acceptBtn.setAttribute("data-application-id",applicationId);
-});
-const rejectModal = document.getElementById('rejectModal');
-rejectModal.addEventListener('show.bs.modal', (event) => {
-  const button = event.relatedTarget; 
-  const applicationId = button.dataset.applicationId; 
-  const rejectBtn = document.querySelector(".rejectConfirmBtn");
-  rejectBtn.setAttribute("data-application-id",applicationId);
+        const acceptModal = document.getElementById('acceptModal');
+        acceptModal.addEventListener('show.bs.modal', (event) => {
+            const button = event.relatedTarget;
+            const applicationId = button.dataset.applicationId;
+            const acceptBtn = document.querySelector(".acceptConfirmBtn");
+            acceptBtn.setAttribute("data-application-id", applicationId);
+        });
+        const rejectModal = document.getElementById('rejectModal');
+        rejectModal.addEventListener('show.bs.modal', (event) => {
+            const button = event.relatedTarget;
+            const applicationId = button.dataset.applicationId;
+            const rejectBtn = document.querySelector(".rejectConfirmBtn");
+            rejectBtn.setAttribute("data-application-id", applicationId);
 
-});
-const scodeModal = document.getElementById('scodeModal');
-scodeModal.addEventListener('show.bs.modal', (event) => {
-  const button = event.relatedTarget; 
-  const applicationId = button.dataset.applicationId; 
-    scodeModal.querySelector('#applicationIdInput').value = applicationId; 
-});
+        });
+        const scodeModal = document.getElementById('scodeModal');
+        scodeModal.addEventListener('show.bs.modal', (event) => {
+            const button = event.relatedTarget;
+            const applicationId = button.dataset.applicationId;
+            scodeModal.querySelector('#applicationIdInput').value = applicationId;
+        });
 
-const table=document.getElementById("adapplication-table");
-table.addEventListener("click",(e)=>
-{
-    if(e.target.classList.contains("scodeEnter"))
-{
-const secretCode= document.getElementById("scode").value;
-const applicationID= document.getElementById("applicationIdInput").value;
+        const table = document.getElementById("adapplication-table");
+        table.addEventListener("click", (e) => {
+            if (e.target.classList.contains("scodeEnter")) {
+                const secretCode = document.getElementById("scode").value;
+                const applicationID = document.getElementById("applicationIdInput").value;
 
-const formData= new FormData();
-formData.append("scode",secretCode);
-formData.append("applicationId",applicationID);
-console.log(formData);
-fetch('/api/passScode',{
-    method:"POST",
-    headers:
-    {
-   
-    },
-    body: formData
-}).then(response=>{return response.json()}
+                const formData = new FormData();
+                formData.append("scode", secretCode);
+                formData.append("applicationId", applicationID);
+                console.log(formData);
+                fetch('/api/passScode', {
+                    method: "POST",
+                    headers:
+                    {
 
-).then(data=>
-{
-    console.log(data);
-    if(data.status == true)
-{
-    const dismissModal=document.getElementById("dismissModal").click();
-const msg="user Registered successfully";
-    showToast(msg);
+                    },
+                    body: formData
+                }).then(response => { return response.json() }
 
-fetch('/api/removeData',{
-    method:"POST",
-    headers:
-    {
+                ).then(data => {
+                    console.log(data);
+                    if (data.status == true) {
+                        const dismissModal = document.getElementById("dismissModal").click();
+                        const msg = "user Registered successfully";
+                        showToast(msg);
 
-    },
-    body:formData
-}).then(response=>{
-    // console.log(response)
-    return response.json()
-})
-.then(data=>
-{
-    console.log(data);
-    if(data.status==true)
-{
-    location.reload();
-}
-}
-)
+                        fetch('/api/removeData', {
+                            method: "POST",
+                            headers:
+                            {
 
-}
-}
-);
-}
-}
-);
+                            },
+                            body: formData
+                        }).then(response => {
+                            // console.log(response)
+                            return response.json()
+                        })
+                            .then(data => {
+                                console.log(data);
+                                if (data.status == true) {
+                                    location.reload();
+                                }
+                            }
+                            )
 
-//accept operation
+                    }
+                }
+                );
+            }
+        });
 
 
-//accept operation
-
-//reject operation
+        //reject operation
 
 
-//reject operation
+        //reject operation
 
 
 
-function showToast(msg)
-{
-var ToastMsg= msg;
-    const toastHtml=`
+        function showToast(msg) {
+            var ToastMsg = msg;
+            const toastHtml = `
 <div class="toast align-items-center text-white bg-primary border-0" role="alert" aria-live="assertive" aria-atomic="true">
   <div class="d-flex">
     <div class="toast-body">
@@ -417,24 +411,24 @@ ${ToastMsg}
     <button type="button" class="btn-close btn-close-white me-2 m-auto" data-bs-dismiss="toast" aria-label="Close"></button>
   </div>
 </div>`;
-    const toastElement = document.createElement('div');
-    toastElement.innerHTML = toastHtml;
-    const toastContainer = document.getElementById('toast-container');
-  toastContainer.appendChild(toastElement);
-  const toast = new bootstrap.Toast(toastElement.querySelector('.toast'),{
-    autohide:true,
-    delay:5000
-  });
+            const toastElement = document.createElement('div');
+            toastElement.innerHTML = toastHtml;
+            const toastContainer = document.getElementById('toast-container');
+            toastContainer.appendChild(toastElement);
+            const toast = new bootstrap.Toast(toastElement.querySelector('.toast'), {
+                autohide: true,
+                delay: 5000
+            });
 
-  toast.show();
-  toastElement.addEventListener('hidden.bs.toast', () => {
-    toastElement.remove();
-    if (toastContainer.children.length === 0) {
-      toastContainer.style.display = 'none';
-    }
-  });
-  toastContainer.style.display = 'block';
-}
+            toast.show();
+            toastElement.addEventListener('hidden.bs.toast', () => {
+                toastElement.remove();
+                if (toastContainer.children.length === 0) {
+                    toastContainer.style.display = 'none';
+                }
+            });
+            toastContainer.style.display = 'block';
+        }
 
     </script>
 </body>
