c26177ed7e939317907f255b1723ff33d0d8106c
oogke <actlikeanut@gmail.com>
2025-02-25 17:57:10 +0545

    feb 25

--- app/Http/Controllers/API/ProjectController.php
+++ app/Http/Controllers/API/ProjectController.php
@@ -15,38 +15,37 @@ class ProjectController extends BaseController
 
     public function projectCreateView()
     {
-$employees= User::all();
-return View('projectManager.projects.createProject',compact('employees'));
+        $employees = User::all();
+        return View('projectManager.projects.createProject', compact('employees'));
 
     }
     public function projectUpdateView(Request $request)
     {
-        $employees= User::all();
-        $projectId= $request->query('projectId');
-$project= Project::where('id',$projectId)->first();
-return View('projectManager.projects.updateProject',compact('project','employees'));
+        $employees = User::all();
+        $projectId = $request->query('projectId');
+        $project = Project::where('id', $projectId)->first();
+        return View('projectManager.projects.updateProject', compact('project', 'employees'));
 
     }
-public function projectDash(Request $req , $id)
-{
-    $project= Project::where('id',$id)->first();
+    public function projectDash(Request $req, $id)
+    {
+        $project = Project::where('id', $id)->first();
 
-    // return view('/projectManager/projects/projectDash',compact('project'));
-
-    return view('/projectManager/projects/projectDash', compact('project'));}
+        return view('/projectManager/projects/projectDash', compact('project'));
+    }
 
     public function projectView()
     {
-        $projects= Project::all();
-        return view('/projectManager/projects/manageProject',compact('projects'));
+        $projects = Project::all();
+        return view('/projectManager/projects/manageProject', compact('projects'));
     }
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
-    $project= Project::all();
-    return $this->sendResponse($project,"All the projects");
+        $project = Project::all();
+        return $this->sendResponse($project, "All the projects");
     }
 
     /**
@@ -58,29 +57,28 @@ public function projectDash(Request $req , $id)
             'name' => 'required|string',
             'description' => 'required|string',
             'sdate' => 'required|date',
-            'edate' => 'required|date', 
+            'edate' => 'required|date',
             'leader' => 'required|array',
             'employee' => 'required|array'
-           
+
         ]);
-        if($validate->fails())
-        {
-            return $this->sendError("Validation Error" ,$validate->errors()->all(),402);
+        if ($validate->fails()) {
+            return $this->sendError("Validation Error", $validate->errors()->all(), 402);
         }
 
 
-        $employee=json_encode($request->employee);
-        $leader=json_encode($request->leader);
-$project=Project::create([
-    'name'=>$request->name,
-    'description'=>$request->description,
-    'sdate'=>$request->sdate,
-    'edate'=>$request->edate,
-    'members'=>$employee,
-   'leader'=> $leader
- 
-]);
-return $this->sendResponse($project,"Project Created Successfully");
+        $employee = json_encode($request->employee);
+        $leader = json_encode($request->leader);
+        $project = Project::create([
+            'name' => $request->name,
+            'description' => $request->description,
+            'sdate' => $request->sdate,
+            'edate' => $request->edate,
+            'members' => $employee,
+            'leader' => $leader
+
+        ]);
+        return $this->sendResponse($project, "Project Created Successfully");
 
     }
 
@@ -89,27 +87,24 @@ return $this->sendResponse($project,"Project Created Successfully");
      */
     public function show(Request $request)
     {
-        $query=Project::query();
-       $projectname=$request->query("name");
-       $id=$request->query("id");
-       if($projectname)
-       {
-$query->where('name','LIKE',"%{$projectname}%");
-       }
-       if($id)
-{
-    $query->where('id','LIKE',"%{$id}%");
+        $query = Project::query();
+        $projectname = $request->query("name");
+        $id = $request->query("id");
+        if ($projectname) {
+            $query->where('name', 'LIKE', "%{$projectname}%");
+        }
+        if ($id) {
+            $query->where('id', 'LIKE', "%{$id}%");
 
-}
+        }
 
 
-$project=$query->get();
-if($project->isEmpty())
-{
-    return $this->sendResponse([],"No data found");
+        $project = $query->get();
+        if ($project->isEmpty()) {
+            return $this->sendResponse([], "No data found");
 
-}
-      return $this->sendResponse($project,"Your result");
+        }
+        return $this->sendResponse($project, "Your result");
 
     }
 
@@ -122,52 +117,47 @@ if($project->isEmpty())
             'name' => 'required|string',
             'description' => 'required|string',
             'sdate' => 'required|date',
-            'edate' => 'required|date', 
+            'edate' => 'required|date',
             'leader' => 'required|array',
-            'employee' => 'required|array'  
+            'employee' => 'required|array'
         ]);
-        if($validate->fails())
-        {
-            return $this->sendError("Validation Error" ,$validate->errors()->all(),402);
+        if ($validate->fails()) {
+            return $this->sendError("Validation Error", $validate->errors()->all(), 402);
         }
-      
-        $employee=json_encode($request->employee);
-        $leader=json_encode($request->leader);
 
-$projectUpdate= Project::where("id",$id)->update([
-    'name'=>$request->name,
-    'description'=>$request->description,
-    'sdate'=>$request->sdate,
-    'edate'=>$request->edate,
-    'members'=>$employee,
-   'leader'=> $leader
-]);
-$project=Project::where('id',$id)->first();
-if($projectUpdate>0)
-{
-return $this->sendResponse($project,"Data is updated");
-}
-else{
-    return $this->sendError("Data is not updated try again",[],402);
-}
+        $employee = json_encode($request->employee);
+        $leader = json_encode($request->leader);
 
-    } 
+        $projectUpdate = Project::where("id", $id)->update([
+            'name' => $request->name,
+            'description' => $request->description,
+            'sdate' => $request->sdate,
+            'edate' => $request->edate,
+            'members' => $employee,
+            'leader' => $leader
+        ]);
+        $project = Project::where('id', $id)->first();
+        if ($projectUpdate > 0) {
+            return $this->sendResponse($project, "Data is updated");
+        } else {
+            return $this->sendError("Data is not updated try again", [], 402);
+        }
+
+    }
     /**
      * Remove the specified resource from storage.
      */
     public function destroy(Request $request)
     {
-        $id=$request->projectId;
-        $project=Project::where('id',$id)->first();
-        if(!$project)
-        {
+        $id = $request->projectId;
+        $project = Project::where('id', $id)->first();
+        if (!$project) {
             return $this->sendError("project not found", [], 404);
         }
-     $query=  $project->delete();
-     if($query)
-     {
-               return $this->sendResponse([],"successfully deleted");
+        $query = $project->delete();
+        if ($query) {
+            return $this->sendResponse([], "successfully deleted");
 
-     }
+        }
     }
 }
